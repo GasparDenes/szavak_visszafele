2d40baadff02dff38a5ed7cdd79ead48e0df35a1
Dénes Gáspár <gdenes007@gmail.com>
2026-03-29 00:04:41 +0100

    Solve task and add gitignore

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.Write("Öt szó vesszővel elválasztva: ");
+        string[] szavak= Console.ReadLine().Split(",");
+        string[] szavak_forditva=new string[5];
+        for (int i = 0; i < 5; i++)
+        {
+            szavak_forditva[i]=Visszafele(szavak[i]);
+            //System.Console.Write($"{Visszafele(szavak[i])} ");
+            System.Console.WriteLine(szavak_forditva[i]); 
+        }
+        
+
+    }
+
+    private static string Visszafele(string szo)
+    {
+        string megforditott="";
+        for (int i = szo.Length-1; i >= 0; i--)
+        {
+            megforditott+=(szo[i]);
+        }
+        return megforditott;
+    }
+}
